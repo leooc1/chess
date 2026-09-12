59a397c18a943c06a896396e30343c33480d7d65
Leonardo Oliveira <leonardoocvrb@gmail.com>
2026-09-12 01:04:15 -0300

    feat: colorir movimentação da peça selecionada

--- src/utils/classes/ChessBoard.ts
+++ src/utils/classes/ChessBoard.ts
@@ -16,9 +16,15 @@ export class ChessBoard {
   clear() {
     this.positions.forEach((rows) => {
       rows.forEach((cp) => {
-        const indicators = cp.querySelectorAll(".absolute.text-black");
-        if (indicators) cp.replaceChildren(...indicators);
-        else cp.replaceChildren();
+        const indicators = cp.querySelectorAll(".position-notation");
+        const possible_moviments = cp.querySelectorAll(".possible-movement");
+        if (indicators || possible_moviments) {
+          !indicators
+            ? cp.replaceChildren(...possible_moviments)
+            : !possible_moviments
+              ? cp.replaceChildren(...indicators)
+              : cp.replaceChildren(...indicators, ...possible_moviments);
+        } else cp.replaceChildren();
       });
     });
   }
@@ -30,23 +36,68 @@ export class ChessBoard {
         piece.getPosition()[1]
       ].insertAdjacentHTML(
         "beforeend",
-        `<div class="absolute w-full h-full flex justify-center items-center">
-      <img class="" src="${piece.getImage()}" />
+        `<div class="piece-position absolute w-full h-full flex justify-center items-center cursor-grab">
+      <img class="piece-image" src="${piece.getImage()}" />
       </div>`,
       );
     }
+    // posiçoes para se mover
+    this.positions.forEach((row, rowIndex) => {
+      row.forEach((col, colIndex) => {
+        if (col.lastElementChild?.classList.contains("piece-position")) {
+          col.lastElementChild.addEventListener("click", () => {
+            const piece = this.pieces.find(
+              (p) =>
+                p.getPosition()[0] == rowIndex &&
+                p.getPosition()[1] == colIndex,
+            );
+            if (piece) {
+              piece.possibleMovements();
+              const otherPieces = this.pieces.filter(
+                (p) =>
+                  p.getPosition()[0] != piece.getPosition()[0] ||
+                  p.getPosition()[1] != piece.getPosition()[1],
+              );
+              otherPieces.forEach((op) => {
+                op.possible_movements = [];
+              });
+            }
+            this.markPossibleMovements();
+          });
+        }
+      });
+    });
+  }
+
+  markPossibleMovements() {
+    const pmElements = document.querySelectorAll(".possible-movement");
+    pmElements.forEach((e) => {
+      e.remove();
+    });
+    this.pieces
+      .map((p) => p.possible_movements)
+      .forEach((pm) => {
+        pm.forEach((p) => {
+          this.positions[p[0]][p[1]].insertAdjacentHTML(
+            "beforeend",
+            `<div class="possible-movement absolute w-full h-full flex justify-center items-center cursor-grab bg-blue-300/40">
+          
+          </div>`,
+          );
+        });
+      });
   }
 
   initialize() {
     // black
     {
+      this.pieces.push(new Rook("black", [0, 0]));
+      this.pieces.push(new Knight("black", [0, 1]));
+      this.pieces.push(new Bishop("black", [0, 2]));
       this.pieces.push(new King("black"));
       this.pieces.push(new Queen("black"));
-      this.pieces.push(new Bishop("black", [0, 2]));
       this.pieces.push(new Bishop("black", [0, 5]));
-      this.pieces.push(new Knight("black", [0, 1]));
       this.pieces.push(new Knight("black", [0, 6]));
-      this.pieces.push(new Rook("black", [0, 0]));
       this.pieces.push(new Rook("black", [0, 7]));
       this.pieces.push(new Pawn("black", [1, 0]));
       this.pieces.push(new Pawn("black", [1, 1]));
@@ -57,17 +108,8 @@ export class ChessBoard {
       this.pieces.push(new Pawn("black", [1, 6]));
       this.pieces.push(new Pawn("black", [1, 7]));
     }
-
     //white
     {
-      this.pieces.push(new King("white"));
-      this.pieces.push(new Queen("white"));
-      this.pieces.push(new Bishop("white", [7, 2]));
-      this.pieces.push(new Bishop("white", [7, 5]));
-      this.pieces.push(new Knight("white", [7, 1]));
-      this.pieces.push(new Knight("white", [7, 6]));
-      this.pieces.push(new Rook("white", [7, 0]));
-      this.pieces.push(new Rook("white", [7, 7]));
       this.pieces.push(new Pawn("white", [6, 0]));
       this.pieces.push(new Pawn("white", [6, 1]));
       this.pieces.push(new Pawn("white", [6, 2]));
@@ -76,6 +118,15 @@ export class ChessBoard {
       this.pieces.push(new Pawn("white", [6, 5]));
       this.pieces.push(new Pawn("white", [6, 6]));
       this.pieces.push(new Pawn("white", [6, 7]));
+      this.pieces.push(new Rook("white", [7, 0]));
+      this.pieces.push(new Knight("white", [7, 1]));
+      this.pieces.push(new Bishop("white", [7, 2]));
+      this.pieces.push(new Queen("white"));
+      this.pieces.push(new King("white"));
+      this.pieces.push(new Bishop("white", [7, 5]));
+      this.pieces.push(new Knight("white", [7, 6]));
+      this.pieces.push(new Rook("white", [7, 7]));
     }
+    this.reload();
   }
 }
