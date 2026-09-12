942a1fe2c29783fa07c09b9ac62c606b763abcd7
Leonardo Oliveira <leonardoocvrb@gmail.com>
2026-09-12 15:32:03 -0300

    feat: logica de movimentação do cavalo

--- src/utils/classes/pieces/Knight.ts
+++ src/utils/classes/pieces/Knight.ts
@@ -3,6 +3,22 @@ import wKnight from "../../../assets/pieces/w-knight.svg";
 import type { Color, Position } from "../../types/types";
 import { Piece } from "../Piece";
 
+function moveLY(positon: Position): Position[] {
+  const upL = [positon[0] - 2, positon[1] - 1] as Position;
+  const upR = [positon[0] - 2, positon[1] + 1] as Position;
+  const downL = [positon[0] + 2, positon[1] - 1] as Position;
+  const downR = [positon[0] + 2, positon[1] + 1] as Position;
+  return [upL, upR, downL, downR];
+}
+
+function moveLX(positon: Position): Position[] {
+  const leftU = [positon[0] - 1, positon[1] - 2] as Position;
+  const leftD = [positon[0] + 1, positon[1] - 2] as Position;
+  const rigthU = [positon[0] - 1, positon[1] + 2] as Position;
+  const rigthD = [positon[0] + 1, positon[1] + 2] as Position;
+  return [leftU, leftD, rigthU, rigthD];
+}
+
 export class Knight extends Piece {
   constructor(color: Color, initial_position: Position) {
     super(
@@ -17,7 +33,13 @@ export class Knight extends Piece {
     if (this.possible_movements.length > 0) {
       this.possible_movements = [];
     } else {
-      this.possible_movements = [this.position];
+      const mvY = moveLY(this.position).filter(
+        (pm) => pm[0] >= 0 && pm[0] <= 7 && pm[1] >= 0 && pm[1] <= 7,
+      );
+      const mvX = moveLX(this.position).filter(
+        (pm) => pm[0] >= 0 && pm[0] <= 7 && pm[1] >= 0 && pm[1] <= 7,
+      );
+      this.possible_movements = [...mvY, ...mvX];
     }
     return this.possible_movements;
   }
