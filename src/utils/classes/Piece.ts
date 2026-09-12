d29c505cd48698cc56aa2bcb623034328b8d3d0b
Leonardo Oliveira <leonardoocvrb@gmail.com>
2026-09-12 20:01:38 -0300

    feat: getters e setters para encapsular

--- src/utils/classes/Piece.ts
+++ src/utils/classes/Piece.ts
@@ -1,11 +1,16 @@
-import type { Color, Position } from "../types/types";
+import type {
+  Color,
+  Position,
+  ResponseCollision,
+  VerifyPosition,
+} from "../types/types";
 
 export class Piece {
   protected color: Color;
   protected initial_position: Position;
   protected position: Position;
   protected image: string;
-  possible_movements: Position[];
+  protected possible_movements: Position[];
 
   constructor(
     color: Color,
@@ -24,11 +29,50 @@ export class Piece {
     return this.position;
   }
 
+  getColor() {
+    return this.color;
+  }
+
   getImage() {
     return this.image;
   }
 
-  possibleMovements(): Position[] {
+  getPossibleMovements() {
+    return this.possible_movements;
+  }
+  setPossibleMovements(possible_movements: Position[]) {
+    return (this.possible_movements = possible_movements);
+  }
+
+  possibleMovements(all_piece_position: VerifyPosition[]): Position[] {
+    all_piece_position;
     return [];
+  }
+
+  verifyPosition(
+    position: Position,
+    all_piece_position: VerifyPosition[],
+  ): ResponseCollision {
+    const search = all_piece_position.find(
+      (p) => p.position[0] == position[0] && p.position[1] == position[1],
+    );
+    if (search) return { color: search.color, found: true };
+    else return { found: false };
+  }
+
+  positionCollision(position: Position, all_piece_position: VerifyPosition[]) {
+    if (this.verifyPosition(position, all_piece_position).found) {
+      if (
+        (this.verifyPosition(position, all_piece_position).color as Color) ==
+        this.color
+      ) {
+        return true;
+      } else {
+        this.possible_movements.push(position);
+        return true;
+      }
+    } else {
+      return false;
+    }
   }
 }
--- src/utils/types/types.ts
+++ src/utils/types/types.ts
@@ -1,2 +1,4 @@
-export type Color = "black" | "white"
-export type Position = [number, number]
+export type Color = "black" | "white";
+export type Position = [number, number];
+export type VerifyPosition = { color: Color; position: Position };
+export type ResponseCollision = { color?: Color; found: Boolean };
