353d79897062ad247bce3ef9d64a3dbaca89ebea
Isaac Max <110857429+JIsaacMax@users.noreply.github.com>
2026-09-17 03:17:26 +0000

    feat: efeitos sonoros para move, killed e start-game (incomplete)

--- src/utils/classes/Game.ts
+++ src/utils/classes/Game.ts
@@ -4,6 +4,19 @@ import { ChessBoard } from "./ChessBoard";
 export class Game {
   protected historicoMovimentacoes: LogMovement[];
   protected chessboard: ChessBoard;
+  private moveSound = [
+    "/sfx/move1.mp3",
+    "/sfx/move2.mp3",
+  ];
+  private captureSound = [
+    "/sfx/capture1.mp3",
+    "/sfx/capture2.mp3",
+  ];
+  private looseSound = [
+    "/sfx/loose.mp3",
+    "/sfx/final-move.mp3",
+  ];
+
   constructor(position_elements: Element[][]) {
     this.historicoMovimentacoes = [];
     this.chessboard = new ChessBoard(position_elements);
@@ -14,16 +27,44 @@ export class Game {
     this.chessboard.set();
   }
 
-  end() {}
+  private getRandomSound(list: string[]) {
+    return list[Math.floor(Math.random() * list.length)]
+  }
+
+  private playSound(type: LogMovement["type"]) {
+    const source = type === "killed" ?
+      this.getRandomSound(this.captureSound)
+      : this.getRandomSound(this.moveSound);
+
+    const sound = new Audio(source);
+    sound.currentTime = 0;
+    sound.play().catch(() => {
+
+    })
+  }
+
+  private playLooseSound() {
+    const source = this.getRandomSound(this.looseSound);
+    const sound = new Audio(source);
+
+    sound.currentTime = 0;
+    sound.play().catch(() => { });
+  }
+
+  end() { }
 
   haveWinner(movimento: LogMovement): Boolean {
     if (movimento.piece == "King" && movimento.type == "died") {
+      this.playLooseSound();
       return true;
     } else return false;
   }
 
   playerMove(movimento: LogMovement) {
     this.historicoMovimentacoes.push(movimento);
+
+    this.playSound(movimento.type);
+
     if (this.haveWinner(movimento))
       alert(
         `Jogados das peças ${movimento.color == "black" ? "brancas" : "pretas"} ganhou!!`,
--- src/utils/initializeChessboard.ts
+++ src/utils/initializeChessboard.ts
@@ -1,11 +1,52 @@
 import { Game } from "./classes/Game";
 
+const soundBoard = new AudioContext();
+
+function playTileSound(index: number, delay = 0) {
+  const oscillator = soundBoard.createOscillator();
+  const gain = soundBoard.createGain();
+  const filter = soundBoard.createBiquadFilter();
+
+  filter.type = "lowpass";
+  oscillator.type = "sine";
+  oscillator.frequency.value = 220 + index * 12;
+
+  const startAt = soundBoard.currentTime + delay;
+
+  gain.gain.setValueAtTime(0.0001, startAt);
+  gain.gain.exponentialRampToValueAtTime(0.12, startAt + 0.02);
+  gain.gain.exponentialRampToValueAtTime(0.0001, startAt + 0.28);
+
+  oscillator.connect(filter);
+  filter.connect(gain);
+  gain.connect(soundBoard.destination);
+
+  oscillator.start(startAt);
+  oscillator.stop(startAt + 0.3);
+}
+
 export default function initializeChessboard() {
   const chessPositions = [...document.querySelectorAll(".chess-position")];
   const matrizChessBoard = [];
+
   for (let i = 0; i < chessPositions.length; i += 8) {
     matrizChessBoard.push(chessPositions.slice(i, i + 8));
   }
+
+  const triggerCascade = () => {
+    if (soundBoard.state === "suspended") {
+      soundBoard.resume();
+    }
+
+    chessPositions.forEach((_, index) => {
+      const delayMs = index * 45;
+      setTimeout(() => {
+        playTileSound(index, 0.01);
+      }, delayMs);
+    });
+  };
+
+  window.addEventListener("pointerdown", triggerCascade, { once: true });
 
   const game = new Game(matrizChessBoard);
   game.start();
